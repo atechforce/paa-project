4256505b58c734c8cdb36c185c24dd489cf9a42f
atechforce <dylansukmayudha@gmail.com>
2026-05-29 16:50:01 +0800

    menambahkan rute map

---
+++ maps.py
@@ -0,0 +1,14 @@
+lokasi = [
+    "taman bekapai",
+    "hotel novotel balikpapan",
+    "hotel grand senyiur balikpapan",
+    "smpn 1 balikpapan"
+]
+
+matriks_jarak = [
+    [0.0, 0.5, 1.2, 2.5],
+    [0.5, 0.0, 0.8, 2.0],
+    [1.2, 0.8, 0.0, 1.5],
+    [2.5, 2.0, 1.5, 0.0]
+]
+
